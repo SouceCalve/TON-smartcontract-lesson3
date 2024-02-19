f5be6352477ae62985c290ec2b38516d1921830f
homayk228 <43516026+homayk228@users.noreply.github.com>
2024-02-19 23:05:17 +0700

    first
    1

---
+++ scripts/deployMain.ts
@@ -0,0 +1,20 @@
+import { toNano } from '@ton/core';
+import { Main } from '../wrappers/Main';
+import { compile, NetworkProvider } from '@ton/blueprint';
+
+export async function run(provider: NetworkProvider) {
+    const main = provider.open(
+        Main.createFromConfig(
+            {
+                res: 0
+            },
+            await compile('Main')
+        )
+    );
+
+    await main.sendDeploy(provider.sender(), toNano('0.05'));
+
+    await provider.waitForDeploy(main.address);
+
+    console.log('Current res: ', await main.getCurrentResValue());
+}
